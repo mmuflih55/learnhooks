84615746adca322be0c04bc805d3523a17e0cf73
apem <m_muflih@hotmail.com>
2019-10-02 09:17:36 +0700

    add drawer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Link } from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { Slide, AppBar, Toolbar, useScrollTrigger, IconButton, ButtonBase } from '@material-ui/core';
+import { Slide, AppBar, Toolbar, useScrollTrigger, IconButton, ButtonBase,List,ListItem,SwipeableDrawer,ListItemIcon,ListItemText } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import {Home,Edit} from '@material-ui/icons';
 import useStyles from '../Style/Style';
+
+ 
 
 const MenuAppBar = () => {
   const classes = useStyles();
   const trigger = useScrollTrigger();
+  const [state,setState] = useState({openDrawer:false})
+
+  const toggleDrawer = (open) => event => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState({ openDrawer:open });
+  };
+
+  function Drawer() {  
+    const sideList = () => (
+        <div
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+          style={{width:250}}
+          >
+          <List>
+            {[{icon:<Home/>,text:'Home',url:'/'},{icon:<Edit/>,text:'Blogs',url:'blogs'}].map((item,i) => (
+              <ListItem button key={i} component={Link} to={item.url}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      );
+    
+      return (
+        <div>
+          <SwipeableDrawer
+            open={state.openDrawer}
+            onClose={toggleDrawer(false)}
+            onOpen={toggleDrawer(true)}
+          >
+            {sideList()}
+          </SwipeableDrawer>
+        </div>
+      );
+    }
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -21,6 +64,7 @@ const MenuAppBar = () => {
               className={classes.menuButton}
               color="inherit"
               aria-label="open drawer"
+              onClick={toggleDrawer(true)}
             >
               <MenuIcon />
             </IconButton>
@@ -41,6 +85,7 @@ const MenuAppBar = () => {
         </AppBar>
       </Slide>
       <Toolbar />
+      {Drawer()}
     </React.Fragment>
   );
 }
